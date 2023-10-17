69d8220c301c4304e096776dba40c086747b1195
Eilla-7 <eilafadel2238@gmail.com>
2023-10-17 14:12:08 -0700

    this file contains a function that get the command path

---
+++ getpath.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * getpath - a function to get the path for the commands
+ * @env: the environment variable
+ * Return: the path value
+ */
+
+char *getpath(char **env)
+{
+	int i = 0;
+	char *value = NULL;
+	char *name = NULL;
+	char *token = NULL;
+	char *delm = "=\n";
+
+	while (env[i] != NULL)
+	{
+		name = strtok(env[i], delm);
+		token = strtok(NULL, delm);
+
+		if (_strcmp(name, "PATH") == 0)
+		{
+			value = malloc(sizeof(char) * (_strlen(token) + 1));
+			_strcpy(value, token);
+			return (value);
+		}
+		i++;
+	}
+	return (NULL);
+}
